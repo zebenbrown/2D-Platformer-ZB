2a67c39eeb393e83279ac0bdaba6b2b77c58c688
zebenbrown <zeben.brown@mymail.champlain.edu>
2024-02-16 14:04:15 -0500

    Started trying to animate the helmet to rotate and also tried to animate the coin

--- Baseball Destroyer/Assets/Scripts/Helmet.cs
+++ Baseball Destroyer/Assets/Scripts/Helmet.cs
@@ -9,6 +9,7 @@ public class Helemt : MonoBehaviour
     [SerializeField] private float lifeTime = 1f;
     private float life = 0f;
     private Rigidbody2D helmetRb;
+    public float rotation;
 
     // Start is called before the first frame update
     void Start()
@@ -19,13 +20,12 @@ public class Helemt : MonoBehaviour
 
     private void FixedUpdate()
     {
-        Vector3 leftInput =  new Vector3(-1, 0, 0);
-        
         helmetRb.AddForce(-transform.right * speed);
     }
     // Update is called once per frame
     void Update()
     {
+        //transform.Rotate(Vector3.forward, rotation * Time.deltaTime);
         life += Time.deltaTime;
         if (life >= lifeTime)
         {
