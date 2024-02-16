964748e860e6729426d49641a344e064bd980f46
Zeben Brown <zeben.brown@mymail.champlain.edu>
2024-02-16 17:21:22 -0500

    - Updated the raycast function, it still has some issues - Added a laser renderer

--- Baseball Destroyer/Assets/Scripts/Projectile.cs
+++ Baseball Destroyer/Assets/Scripts/Projectile.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Net;
 using Unity.IO.LowLevel.Unsafe;
 using UnityEngine;
 
@@ -8,24 +9,43 @@ public class Projectile : MonoBehaviour
 {
     [SerializeField] private Transform spawnLocation;
     [SerializeField] private GameObject helmetPrefab;
+    
     private bool canFire = true;
-    public LayerMask layersToHit;
+    //[SerializeField] private LayerMask layerMask;
+    [SerializeField] private LayerMask playerLayerMask;
     private RaycastHit2D hit2D;
+    [SerializeField] private LineRenderer render;
+    [SerializeField] private float maxDistance = 10f;
     
     private void FixedUpdate()
     {
-        hit2D = Physics2D.Raycast(transform.position, Vector3.left, 100);
+        Vector3 endPoint = transform.position + transform.right * -maxDistance;
+        hit2D = Physics2D.BoxCast(transform.position, Vector2.one * maxDistance, 0, endPoint, maxDistance);
+        
+        //if (hit2D.collider != null)
+        //{
+        //    endPoint = hit2D.collider;
+        //}hit2D collider
+        //render.positionCount = 2;
+        //render.SetPositions(new Vector3[] {transform.position, endPoint});
+
+        if (hit2D.collider.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+                if (canFire)
+                {
+                             canFire = false;
+                                     
+                                 if (hit2D)
+                                 {
+                                     Fire();
+                                     StartCoroutine(HelmetSpawnDelay());
+                                 }
+                }
+                Debug.Log("Hit player");
+        }
+        
         //If enemy can fire
-            if (canFire)
-            {
-                canFire = false;
-                        
-                    if (hit2D)
-                    {
-                        Fire();
-                        StartCoroutine(HelmetSpawnDelay());
-                    }
-            }
+           
     }
     
     //Function for Helmet instantitation
