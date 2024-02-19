2ad32e8fe4d93ad458bf75276fffa704f51a8c9a
zebenbrown <zeben.brown@mymail.champlain.edu>
2024-02-19 18:41:39 -0500

    Changed the projectile script a little bit

--- Baseball Destroyer/Assets/Scripts/Enemy.cs
+++ Baseball Destroyer/Assets/Scripts/Enemy.cs
@@ -21,35 +21,34 @@ public class Enemy : MonoBehaviour
       currentPoint = movePoints[pointIndex];
    }
    //Enemy Movement
-   private void FixedUpdate()
-   {
-      transform.position = Vector2.MoveTowards(transform.position, currentPoint.position, moveSpeed);
-      if (Vector2.Distance(transform.position, currentPoint.position) < 0.01f)
-      {
-         pointIndex++;
-         pointIndex %= movePoints.Length;
-         currentPoint = movePoints[pointIndex];
-         if (currentPoint == movePoints[0])
-         {
-            GetComponent<SpriteRenderer>().flipX = true;
-            //If player in box cast flip enemy
-            if (projectile.playerDetected)
-            {
-               GetComponent<SpriteRenderer>().flipX = false;
-            }
-            
-            else if (projectile.playerDetected)
-            {
-               GetComponent<SpriteRenderer>().flipX = false;
-            }
-            
-         }
-         else
-         {
-            GetComponent<SpriteRenderer>().flipX = false;
-         }
-      }
-   }
+    private void FixedUpdate()
+    {
+       transform.position = Vector2.MoveTowards(transform.position, currentPoint.position, moveSpeed);
+       if (Vector2.Distance(transform.position, currentPoint.position) < 0.01f)
+       {
+          pointIndex++;
+          pointIndex %= movePoints.Length;
+          currentPoint = movePoints[pointIndex];
+       if (currentPoint == movePoints[0])
+       {
+          //If player in box cast flip enemy
+          if (projectile.playerDetected)
+          {
+             GetComponent<SpriteRenderer>().flipX = false;
+          }
+          else
+          {
+             GetComponent<SpriteRenderer>().flipX = true;
+          }
+       }
+       else
+       {
+          GetComponent<SpriteRenderer>().flipX = false;
+       }
+       }
+    }
+   
+   
 }
 
 
